8e5f9c796bd11c16ab96aa766a6872500f50c937
Sam Breed <sam@quickleft.com>
2013-03-11 01:38:25 -0600

    fixing bugs and commenting in getUntaken

--- app/collections/taken.js
+++ app/collections/taken.js
@@ -33,12 +33,21 @@ define(function(require, exports, module){
       return obj.calendars[ this.config.get('calendar') ].busy;
     },
 
+    // Google only returns your "busy" times, so we need to transform those
+    // into your available times, in the format:
+    //
+    //    * Monday, 1/23 - 4 to 6pm
+    //    * Tuesday, 1/24 - 11am to 1pm
+    //
+    // returns an array of timestrings
     getUntaken: function(){
-      var ret = this.map(function(model, i, list){
+      var timeblock = this.map(function(model, i, list){
+        // Convert the start and end times in the model to local moment objects
         var start = moment( model.get('start') ).local();
         var end   = moment( model.get('end') ).local();
         var next, nextEnd;
 
+        // If there are more model left, look ahead to the next model
         if( i + 1 === this.length ){
           next = end;
         } else {
@@ -46,18 +55,28 @@ define(function(require, exports, module){
           nextEnd = moment( this.at(i + 1).get('end') ).local();
         }
 
-        var dayStart = start.startOf('day').add('hours', START_TIME);
+        // Set the default starting time
+        // TODO: this should be configurable
+        var dayStart = moment( model.get('start') ).startOf('day').add('hours', START_TIME);
 
-        // begin availbaility block with either the beging of the day or the end of the first meeting
-        var beginning = start.hour() - dayStart.hours() > 0 ? dayStart : end;
+        // Begin availbaility block with the start of the day if possible,
+        // other wise start with the end of the current meeting
+        var beginning = start.hour() - dayStart.hour() < 0 ? dayStart : end;
 
+        // The end of the current available time block is either the start of
+        // the next "busy" time or the end of the current day
         var ending = next.date() === beginning.date() ?
           next : start.startOf('day').add('hours', END_TIME);
 
+        // If the beginning and the ending happen to be *exactly* equal (this
+        // happens, confusingly enough,) make sure the time block ends at the
+        // end of the current day
         if( beginning === ending ){
           ending = start.startOf('day').add('hours', END_TIME);
         }
 
+        // Make sure that am/pm suffixes are only applied when they differ
+        // between start and end times
         var beginningFormat = beginning.minutes() === 0 ? 'h' : 'h:mm';
         var endFormat = ending.minutes() === 0 ? 'ha' : 'h:mma';
 
@@ -65,16 +84,16 @@ define(function(require, exports, module){
           beginningFormat += 'a';
         }
 
-        var timeblock = beginning.format('dddd M/D - '+ beginningFormat +' to ') + ending.format(endFormat);
-
-        return timeblock;
+        // Format a timestring in the form: "Monday, 1/23 - 4 to 6pm"
+        var timestring = beginning.format('dddd M/D - '+ beginningFormat +' to ') + ending.format(endFormat);
+        return timestring;
       }, this);
 
-      if( ret.length === 0 ){
-        ret = ["You're the jerk that doesn't have anything scheduled.", "Good for you."];
+      if( timeblock.length === 0 ){
+        timeblock = ["You're the jerk that doesn't have anything scheduled.", "Good for you."];
       }
 
-      return ret;
+      return timeblock;
     }
   });
 });
