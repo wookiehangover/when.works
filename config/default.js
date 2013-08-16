6248674d22e55e49389e6e2bc5f3e820e2ec78b7
Sam Breed <sam@quickleft.com>
2013-08-15 23:44:00 -0600

    changes for deploy

---
+++ config/default.js
@@ -0,0 +1,25 @@
+/*jshint asi:true */
+exports.google = {
+  id: process.env.GOOGLE_ID || '702998348467.apps.googleusercontent.com',
+  secret: process.env.GOOGLE_SECRET || 'HzRW_uyVwOI2UlM1MEqY8KDP'
+}
+
+exports.secret = process.env.SECRET || 'shhh this should be a secret 8FNDSJKFNDUu9889FJDSFJKSDB'
+
+exports.less = {
+  src: './public/less'
+}
+
+exports.redis = {
+  host: process.env.REDIS_HOST || '127.0.0.1',
+  port: 6379
+}
+
+if( process.env.REDIS_PASS ){
+  exports.redis.pass = process.env.REDIS_PASS
+}
+
+exports.requirejs = {
+  path: '/js/untaken.js',
+  source: '/app/components/requirejs/require.js'
+}
