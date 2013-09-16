49901ec9c944ae96a8427f7fe4540bff289dafcf
Sam Breed <sam@quickleft.com>
2013-09-15 23:24:44 -0600

    adding a redirect to unavailable.at

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@
  */
 
 exports.index = function(req, res){
+  if( req.headers && String(req.headers.host).indexOf('untaken.herokuapp.com') > - 1 ){
+    return res.redirect('http://unavailable.at');
+  }
   var user = req.session ? req.session.user : false;
   res.render('index', { user: user });
 };
