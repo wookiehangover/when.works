8629f838b3f8b9dbd1d7056e455c7834990db04a
Sam Breed <sam@quickleft.com>
2014-05-18 11:07:21 -0600

    comments for time-merge algorithm

--- app/collections/timeblocks.js
+++ app/collections/timeblocks.js
@@ -5,10 +5,30 @@ define(function(require, exports, module) {
 
   module.exports = Backbone.Collection.extend({
 
-    getTimesByDay: function(){
-      var timeblocks = this.pluck('times');
-      var days = {};
+    // Create a single list of free times across all calendars in the
+    // collection
+    //
+    // [
+    //   [[meetingStart, meetingEnd], ...]
+    //   ...
+    // ]
+    //
+    reduceTimes: function() {
+      var days = this.getTimesByDay(this.pluck('times'));
 
+      return _.map(days, function(dayblock) {
+        return this.filterDayblock(dayblock.sort(function(a, b) {
+          return a[0] - b[0];
+        }));
+      }, this);
+    },
+
+    // Merge timeblocks from all calendars into a single object, keyed by date
+    //
+    // {
+    //   2014-01-01: [timeblocks]
+    // }
+    getTimesByDay: function(timeblocks){
       return _.reduce(timeblocks, function(days, timeblock){
         _.each(timeblock, function(times, day){
           if (days[day]) {
@@ -21,33 +41,50 @@ define(function(require, exports, module) {
       }, {});
     },
 
+    // Removes duplicate and overlapping time entries in a pre-merged dayblock,
+    // creating a single list of availability for multiple calendars.
+    //
+    // Assumes that the `dayblock` provided has more than 1 calendar, otherwise
+    // this is just an expensive noop.
+    //
+    // Returns an array of available times:
+    //
+    // [
+    //   [meetingStart, meetingEnd],
+    //   ...
+    // ]
     filterDayblock: function(dayblock) {
+      // Keeps track of merged nodes as to not add an index that's been merged
       var merged = [];
-      var mergedList = [];
-
-      _.each(dayblock, function(time, i){
+      // Reduce the list of times, merging overlapping nodes
+      //
+      // Given: [[a, b], [c, d]] where B > C
+      //  create a merged entry of the form: [a, (B || D)]
+      var mergedList = _.reduce(dayblock, function(block, time, i){
         var next = dayblock[i + 1];
         if (next && time[1].isAfter(next[0])) {
-          mergedList.push([
+          block.push([
             next[0],
             (next[1].isAfter(time[1]) ? next[1] : time[1] )
           ]);
           merged.push(i + 1);
         } else if (!(i in merged)) {
-          mergedList.push(time);
+          block.push(time);
         }
-      });
+        return block;
+      }, []);
 
+      // Walk through the merged list, pruning any meeting slots that are still
+      // colliding
+      //
+      // TODO: this seems like a klude, but it seems to work, soo...
       return _.reject(mergedList, function(time, i){
         var next = mergedList[i + 1];
-        if (next) {
-          return this.detectMeetingCollision(time, next);
-        } else {
-          return false;
-        }
+        return next ? this.detectMeetingCollision(time, next) : false;
       }, this);
     },
 
+    // Helper for improving readability around meeting start/end comparisons
     detectMeetingCollision: function(first, next) {
       if (first[1].isAfter(next[0])) {
         return true;
@@ -59,18 +96,6 @@ define(function(require, exports, module) {
         return true;
       }
       return false;
-    },
-
-    reduceTimes: function() {
-      var days = this.getTimesByDay();
-
-      return _.map(days, function(dayblock) {
-        dayblock = dayblock.sort(function(a, b) {
-          return a[0] - b[0];
-        });
-
-        return this.filterDayblock(dayblock);
-      }, this);
     }
   });
 
