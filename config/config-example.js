fc3ac7c4ffddccf01606697bd2a9c0f731ac368f
Sam Breed <sam@quickleft.com>
2014-06-22 14:26:00 -0600

    updating example config

--- config/config-example.js
+++ config/config-example.js
@@ -1,11 +1,32 @@
 /*jshint asi:true */
 exports.google = {
-  id: '',
-  secret: ''
+  id: process.env.GOOGLE_ID || '',
+  secret: process.env.GOOGLE_SECRET || ''
 }
 
-exports.secret = 'shhh this should be a secret 8FNDSJKFNDUu9889FJDSFJKSDB'
+exports.secret = process.env.SECRET || ''
 
 exports.less = {
   src: './public/less'
 }
+
+exports.redis = {
+  host: process.env.REDIS_HOST || '127.0.0.1',
+  port: process.env.REDIS_PORT || 6379
+}
+
+if( process.env.REDIS_PASS ){
+  exports.redis.pass = process.env.REDIS_PASS
+}
+
+exports.requirejs = {
+  path: '/js/untaken.js',
+  source: '/app/components/requirejs/require.js'
+}
+
+exports.browserify = {
+  dev: '/js/untaken.js',
+  prod: '/js/untaken.min.js'
+}
+
+exports.cacheExpires = parseInt(process.env.CACHE_EXPIRES, 10) || 1;
