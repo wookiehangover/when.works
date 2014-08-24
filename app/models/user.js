d2582eb4182270c93f139e7c5afbc5b5fc9680a4
Sam Breed <sam@quickleft.com>
2014-08-23 18:47:35 -0600

    adding setAuthHeader to user model

--- app/models/user.js
+++ app/models/user.js
@@ -1,6 +1,15 @@
+var $ = require('jquery');
 var Backbone = require('backdash');
 var BASE_URL = process.env.BASE_URL || '';
 
 module.exports = Backbone.Model.extend({
-  url: BASE_URL + '/me'
+  url: BASE_URL + '/me',
+
+  setAuthToken: function(authToken) {
+    $.ajaxSetup({
+      beforeSend: function(req) {
+        req.setRequestHeader('auth_token', authToken);
+      }
+    });
+  }
 });
