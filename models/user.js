89d010858bf52acd346fd42cc2348dd246b5bbf3
Sam Breed <sam@quickleft.com>
2014-09-21 17:27:54 -0600

    make mandrill api return a promise

--- models/user.js
+++ models/user.js
@@ -3,7 +3,10 @@ var thinky = require('thinky')(config.rethinkdb)
 
 var User = thinky.createModel('User', {
   id: String,
-  settings: Object
+  settings: Object,
+  daily_digest: Boolean
 })
+
+User.ensureIndex('daily_digest')
 
 module.exports = User
